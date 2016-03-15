92819004d1bf29fab2e7e6c5fd8084ce438a36f7
Sean O'Hollaren <Sean.OHollaren2@nike.com>
2016-03-14 18:26:39 -0700

    Initial functionality working with some param validation and help info

--- index.js
+++ index.js
@@ -5,33 +5,75 @@ const spawn = require('child_process').spawn;
 
 // Grab values from flags
 // TODO: Add more flags to override default values
-const appName = argv.name;
-const appPath = argv.path;
+const appName = argv.name || argv.n;
+const appPath = argv.path || argv.p;
 
-// set appropriate environment variables
+// TODO: Look for missing or malformed args and bail early with an informative error message
+if(argv.help) {
+  printHelp();
+  process.exit(1);
+}
+
+if (!appName) {
+  console.log(`\n  Missing app name.  Specify with the --name flag. \n\n  Exiting... \n`);
+  process.exit(1);
+}
+
+if (!appPath) {
+  console.log(`\n  Missing path to the app you want to run with nsolid.  Specify with the --path flag. \n\n  Exiting... \n`);
+  process.exit(1);
+}
+
+console.log(`\n  Launching app: ${appName}`);
+
+// Set appropriate environment variables
 process.env.NSOLID_APPNAME = appName;
 // TODO: Allow these to be overridden by defaults
 process.env.NSOLID_HUB = 'localhost:4001';
 process.env.NSOLID_SOCKET = 1111;
 
-// define strings to start up child processes
+// Define strings to start up child processes
 let etcdExec = `etcd`;
 let etcdArgs =  [`-name`, `nsolid_proxy`, `-listen-client-urls`, `http://0.0.0.0:4001`, `-advertise-client-urls`, `http://0.0.0.0:4001`, `-initial-cluster-state`, `new`];
-let proxyExec = `nsolid nsolid/nsolid-proxy/proxy.js`;
-let consoleExec = `node nsolid/nsolid-console/bin/nsolid-console --interval=1000`
 
+// TODO: Allow the location of the proxy files to be specified?
+let proxyExec = `node`;
+let proxyArgs =  [`nsolid/nsolid-proxy/proxy.js`];
+
+// TODO: Allow the location of the console files to be specified?
+let consoleExec = `node`;
+let consoleArgs =  [`nsolid/nsolid-console/bin/nsolid-console`, `--interval=1000`];
+
+// Start up target app with nsolid
+let appExec = `nsolid`;
+let appArgs = [appPath];
+
+// Array to hold all child processes
 let children = [];
 
-// spawn child processes and add to children array
-console.log(`Spawning etcd...`);
+// Spawn child processes and add to children array
 children.push(spawn(etcdExec, etcdArgs));
-// console.log(`Spawning nsolid proxy...`);
-// children.push(spawn(proxyExec));
-// console.log(`Spawning nsolid console...`);
-// children.push(spawn(consoleExec));
+children.push(spawn(proxyExec, proxyArgs));
+children.push(spawn(consoleExec, consoleArgs));
+children.push(spawn(appExec, appArgs));
 
-// pipe output and err through current process
+// Pipe output and err through current process
 children.forEach(child => {
   child.stdout.pipe(process.stdout);
   child.stderr.pipe(process.stderr);
-})
+});
+
+function printHelp() {
+  console.log(`\n N|Solid Manager
+
+    Sets up the prerequisites for an N|Solid server and executes a target app using nsolid.
+
+    Arguments:
+
+      --name    Name of the app as you'd like it to appear in the N|Solid console
+      --path    Path to the target app's entry point
+
+    Example:
+
+      node index --name myApp --path ../code/myApp/app.js \n`);
+}
