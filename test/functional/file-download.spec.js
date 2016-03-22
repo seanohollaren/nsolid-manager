4bbc54991b6d565e025ce7054e23468ea687cf02
Alex Wolden <woldena@ochin.org>
2016-03-21 20:21:27 -0700

    added basic functional test for file downlaoder

---
+++ test/functional/file-download.spec.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const FileDownloader = require('../../lib/file-downloader');
+const path = require('path');
+const fs = require('fs');
+const expect = require('chai').expect;
+
+/*
+This currently taking a bit long to run.
+We should have smaller test files we can use.
+*/
+
+
+
+describe('FileDownloader Works', function () {
+
+  it('Should Work Without A Checksum', function (done) {
+    this.timeout(60000);
+    const location = path.resolve(__dirname, '../ignore/nsolid-proxy-v3.4.2.tar.gz');
+
+
+    /*
+    Test Without Checksum
+    */
+    let testDownloader = new FileDownloader({
+        url: 'https://nsolid-download.nodesource.com/download/nsolid-proxy/release/v3.4.2/nsolid-proxy-v3.4.2.tar.gz',
+        location
+      })
+      .on('progress', (stat) => {
+        expect(stat).to.be.an('object');
+        expect(stat.time).to.be.an('object');
+      })
+      .on('done', (loc) => {
+        expect(loc).to.equal(location);
+        done();
+      })
+      .start();
+
+  });
+
+  it('Should Work With A Checksum', function (done) {
+    this.timeout(60000);
+    const location = path.resolve(__dirname, '../ignore/nsolid-proxy-v3.4.2.tar.gz');
+
+
+    /*
+    Test Without Checksum
+    */
+    let testDownloader = new FileDownloader({
+        url: 'https://nsolid-download.nodesource.com/download/nsolid-proxy/release/v3.4.2/nsolid-proxy-v3.4.2.tar.gz',
+        location,
+        checksum: '615a24727846612c87c2240b603d09d0eafc1f5100e5f28ce20a3121ca1b719d'
+      })
+      .on('progress', (stat) => {
+        expect(stat).to.be.an('object');
+      })
+      .on('done', (loc) => {
+        expect(loc).to.equal(location);
+        done();
+      })
+      .on('error', (error) => {
+        expect(error).to.equal(undefined);
+      })
+      .start();
+
+  });
+
+});
